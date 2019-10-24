50be108012014eab282219f7f283764611ed6d9c
Karelin Grigoriy <grundiss@squadrille.com>
2019-10-24 22:12:50 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function App() {
+  return <div>there we go</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
